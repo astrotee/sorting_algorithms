489614e099260657031724e2eca292b826a4ccbc
amin ahmed <aminahmed24@gmail.com>
2024-02-17 13:06:40 +0200

    0. Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+* bubble_sort - sorting algorithm
+* @array: the array to sort
+* @size: the array size
+* Return: Nothing
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, n = size, nn;
+	int tmp;
+
+	do {
+		nn = 0;
+		for (i = 1; i < n; i++)
+		{
+			if (array[i] > array[i - 1])
+				continue;
+			tmp = array[i];
+			array[i] = array[i - 1];
+			array[i - 1] = tmp;
+			nn = i;
+			print_array(array, size);
+		}
+		n = nn;
+	} while (n > 0);
+}
---
+++ Makefile
@@ -0,0 +1,4 @@
+all: bubble
+
+bubble: 0-bubble_sort.c 0-main.c print_array.c
+	gcc -Wall -Wextra -Werror -pedantic  -std=gnu89 0-bubble_sort.c 0-main.c print_array.c -o bubble
